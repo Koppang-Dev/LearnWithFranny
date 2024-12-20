6a9ce26d4b77a54f1a2702939f4ca2280f05b555
Riley <koppangdev@gmail.com>
2024-12-20 16:36:04 -0700

    Added spring boot initalizer

---
+++ backend/LearnWithFranny/src/main/java/com/LearnWithFranny/LearnWithFranny/LearnWithFrannyApplication.java
@@ -0,0 +1,13 @@
+package com.LearnWithFranny.LearnWithFranny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnWithFrannyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnWithFrannyApplication.class, args);
+	}
+
+}
---
+++ backend/LearnWithFranny/src/test/java/com/LearnWithFranny/LearnWithFranny/LearnWithFrannyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LearnWithFranny.LearnWithFranny;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnWithFrannyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
