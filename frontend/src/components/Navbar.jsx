2b517afec082baf4735713543c0acf178d6953aa
Riley <koppangdev@gmail.com>
2025-01-06 14:59:04 -0700

    Finished the NavBar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,40 @@
+import Image from "next/image";
+
 const Navbar = () => {
-  return <div></div>;
+  return (
+    <div className="flex items-center justify-between p-4">
+      {/* SEARCH BAR */}
+      <div className="hidden md:flex item-center gap-2 text-sm rounded-full ring-[1.5px] ring-gray-300 px-2 ">
+        <Image src="/images/search.png" alt="" width={14} height={14} />
+        <input
+          type="text"
+          placeholder="Search..."
+          className="w-[200px] p-2 bg-transparent outline-none"
+        />
+      </div>
+
+      {/* ICONS AND USER */}
+      <div className="flex items-center gap-6 justify-end w-full">
+        <div className="bg-white rounded-full w-7 h-7 flex items-center justify-center cursor-pointer">
+          <Image src="/images/message.png" alt="" width={20} height={20} />
+        </div>
+        <div className="bg-white rounded-full w-7 h-7 flex items-center justify-center cursor-pointer">
+          <Image src="/images/announcement.png" alt="" width={20} height={20} />
+        </div>
+        <div className="flex flex-col">
+          <span className="text-xs leading-3 font-medium">John Doe</span>
+          <span className="text-[10px] text-gray-500 text-right">Student</span>
+        </div>
+        <Image
+          src="/images/avatar.png"
+          alt=""
+          width={36}
+          height={36}
+          className="rounded-full"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
