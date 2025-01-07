a93f786cdc2d0d10832be628d67b9bddbcbbcb78
Riley <koppangdev@gmail.com>
2025-01-07 13:37:59 -0700

    Made the first activity chart

--- frontend/src/components/ActivityChart.jsx
+++ frontend/src/components/ActivityChart.jsx
@@ -1,6 +1,97 @@
 "use client";
+import React, { PureComponent } from "react";
+import Image from "next/image";
+import {
+  BarChart,
+  Bar,
+  Rectangle,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+
+const data = [
+  {
+    name: "Mon",
+    quizzes: 1,
+    tests: 2,
+  },
+  {
+    name: "Tue",
+    quizzes: 10,
+    tests: 1,
+  },
+  {
+    name: "Wed",
+    quizzes: 4,
+    tests: 4,
+  },
+  {
+    name: "Thu",
+    quizzes: 6,
+    tests: 6,
+  },
+  {
+    name: "Fri",
+    quizzes: 1,
+    tests: 2,
+  },
+  {
+    name: "Sat",
+    quizzes: 0,
+    tests: 0,
+  },
+  {
+    name: "Sun",
+    quizzes: 1,
+    tests: 1,
+  },
+];
+
 const ActivityChart = () => {
-  return <div className=""></div>;
+  return (
+    <div className="bg-white rounded-lg p-4 h-full">
+      <div className="flex justify-between items-center">
+        <h1 className="text-lg font-semibold">Activity</h1>
+        <Image src="/images/more.png" alt="" width={20} height={20} />
+      </div>
+      <ResponsiveContainer width="100%" height="90%">
+        <BarChart width={500} height={300} data={data} barSize={20}>
+          <CartesianGrid strokeDasharray="3 3" vertical={false} stroke="#ddd" />
+          <XAxis
+            dataKey="name"
+            axisLine={false}
+            tick={{ fill: "#d1d5db" }}
+            tickLine={false}
+          />
+          <YAxis axisLine={false} tick={{ fill: "#d1d5db" }} tickLine={false} />
+          <Tooltip
+            contentStyle={{ borderRadius: "10px", borderColor: "lightgray" }}
+          />
+          <Legend
+            align="left"
+            verticalAlign="top"
+            wrapperStyle={{ paddingTop: "20px", paddingBottom: "40px" }}
+          />
+          <Bar
+            dataKey="tests"
+            fill="#FAE27C"
+            legendType="circle"
+            radius={[10, 10, 0, 0]}
+          />
+          <Bar
+            dataKey="quizzes"
+            fill="#C3EBFA"
+            legendType="circle"
+            radius={[10, 10, 0, 0]}
+          />
+        </BarChart>
+      </ResponsiveContainer>
+    </div>
+  );
 };
 
 export default ActivityChart;
