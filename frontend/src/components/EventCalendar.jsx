4750da0a1831e92993c7e0561d0811ee3f37b3b6
Riley <koppangdev@gmail.com>
2025-01-09 20:39:53 -0700

    Finished the event calendar

--- frontend/src/components/EventCalendar.jsx
+++ frontend/src/components/EventCalendar.jsx
@@ -2,6 +2,7 @@
 import Calendar from "react-calendar";
 import { useState } from "react";
 import "react-calendar/dist/Calendar.css";
+import Image from "next/image";
 
 // TEMPORARY
 // Events below the calendar
@@ -33,14 +34,21 @@ const EventCalendar = () => {
   return (
     <div className="bg-white p-4 rounded-md ">
       <Calendar onChange={onChange} value={value} />
+      <div className="flex items-center justify-between">
+        <h1 className="text-lg font-semibold my-4 ">Events</h1>
+        <Image src="/images/more.png" alt="" width={20} height={20} />
+      </div>
       <div className="flex flex-col gap-4">
         {events.map((event) => (
-          <div className="" key={event.id}>
+          <div
+            className="p-5 rounded-md border-2 border-gray-100 border-t-4 odd:border-t-lamaSky even:border-t-lamaPurple"
+            key={event.id}
+          >
             <div className="flex items-center justify-between">
-              <h1>{event.title}</h1>
-              <span>{event.time}</span>
+              <h1 className="font-semibold text-gray-600">{event.title}</h1>
+              <span className="text-gray-300 text-xs">{event.time}</span>
             </div>
-            <p>{event.description}</p>
+            <p className="mt-2 text-gray-400 text-sm">{event.description}</p>
           </div>
         ))}
       </div>
