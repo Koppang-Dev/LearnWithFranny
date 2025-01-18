c2343759b4f87411db20425a25385188d7fd7e33
Riley <koppangdev@gmail.com>
2025-01-18 16:50:17 -0700

    Added testimonals into an array for future use

--- frontend/src/app/(landingPage)/user-stats-section.jsx
+++ frontend/src/app/(landingPage)/user-stats-section.jsx
@@ -38,12 +38,66 @@ const tabs = [
     icon: <TbSwitch3 className="text-3xl mr-2 text-sky-600 rounded-md" />,
     title: "Visualize, filter and sort any way you want",
     description: "Mark tasks for your study group and assign readings",
-
-    //   Stopped at 4:12
+    images: [
+      {
+        title: "Record Lectures and Transform them into notes",
+        picture: "/assets/DogJumpDoodle.svg",
+      },
+      {
+        title: "Share Notes and Folders with your friends",
+        picture: "/assets/DoogieDoodle.svg",
+      },
+      {
+        title:
+          "Use various different study methods to achieve the grade you deserve",
+        picture: "/assets/FloatDoodle.svg",
+      },
+    ],
+  },
+  {
+    profile: "assets/DoogieDoodle.svg",
+    userName: "Carlos Hernandez",
+    userHandle: "@carlos",
+    userText: (
+      <div className="md:mt-6">
+        <span className="text-sky-500">@LearnWithFranny</span>
+        it is definitely the best tool I have ever used
+      </div>
+    ),
+  },
+  {
+    profile: "assets/DoogieDoodle.svg",
+    userName: "John Wright",
+    userHandle: "@jwright",
+    userText: (
+      <div className="md:mt-6">
+        <span className="text-sky-500">@LearnWithFranny</span>
+        It helped sooo much with studying!
+      </div>
+    ),
+  },
+  {
+    profile: "assets/DoogieDoodle.svg",
+    userName: "Makayla Palmer",
+    userHandle: "@Makalayaya",
+    userText: (
+      <div className="md:mt-6">
+        <span className="text-sky-500">@LearnWithFranny</span>
+        It saved me contless hours having all of this into one!
+      </div>
+    ),
   },
 ];
 
 const UserStatsSection = () => {
+  const ref = useRef(null);
+
+  // Setting Active Image
+  const [activeImageIndex, setActiveImageIndex] = useState(0);
+  const handleDotClick = (index) => {
+    setActiveImageIndex(index);
+  };
+
   return <div></div>;
 };
 
