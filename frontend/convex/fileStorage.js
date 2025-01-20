7ca276076711a11d34c6acd1968eb9e07f5f6da2
Riley <koppangdev@gmail.com>
2025-01-20 14:56:08 -0700

    File uploading into convex is now working

--- frontend/convex/fileStorage.js
+++ frontend/convex/fileStorage.js
@@ -1,5 +1,26 @@
+import { v } from "convex/values";
 import { mutation } from "./_generated/server";
 
 export const generateUploadUrl = mutation(async (ctx) => {
   return await ctx.storage.generateUploadUrl();
+});
+
+export const AddFileEntryToDb = mutation({
+  args: {
+    fileId: v.string(),
+    storageId: v.string(),
+    fileName: v.string(),
+    userEmail: v.string(),
+    createdBy: v.string(),
+  },
+  handler: async (ctx, args) => {
+    const result = await ctx.db.insert("files", {
+      fileId: args.fileId,
+      fileName: args.fileName,
+      storageId: args.storageId,
+      userEmail: args.userEmail,
+      createdBy: args.createdBy,
+    });
+    return "Inserted";
+  },
 });
--- frontend/src/app/uploadNote/_components/UploadScreen.jsx
+++ frontend/src/app/uploadNote/_components/UploadScreen.jsx
@@ -14,13 +14,19 @@ import { DialogClose } from "@radix-ui/react-dialog";
 import { useMutation } from "convex/react";
 import { Loader2Icon } from "lucide-react";
 import { useState } from "react";
-import { generateUploadUrl } from "../../../../convex/fileStorage";
+import uuid4 from "uuid4";
+import {
+  AddFileEntryToDb,
+  generateUploadUrl,
+} from "../../../../convex/fileStorage";
 import { api } from "../../../../convex/_generated/api";
 
 const UploadScreen = ({ children }) => {
   const generateUploadUrl = useMutation(api.fileStorage.generateUploadUrl);
+  const InsertFileEntry = useMutation(api.fileStorage.AddFileEntryToDb);
   const [file, setFile] = useState();
   const [loading, setLoading] = useState(false);
+  const [fileName, setFileName] = useState();
 
   // File is selected
   const OnFileSelect = (event) => {
@@ -48,6 +54,16 @@ const UploadScreen = ({ children }) => {
     console.log("Storage ID", storageId);
 
     // Saving the storage ID into the database
+    const fileId = uuid4();
+    const response = await InsertFileEntry({
+      fileId: fileId,
+      storageId: storageId,
+      fileName: fileName ?? "Untitiled File",
+      userEmail: "User@gmail.com",
+      createdBy: "User's Name",
+    });
+
+    console.log(response);
 
     // Set loading icon back to false
     setLoading(false);
@@ -73,7 +89,10 @@ const UploadScreen = ({ children }) => {
                 </div>
                 <div className="mt-2">
                   <label>FIle Name *</label>
-                  <Input placeholder="File Name" />
+                  <Input
+                    placeholder="File Name"
+                    onChange={(e) => setFileName(e.target.value)}
+                  />
                 </div>
               </div>
             </DialogDescription>
