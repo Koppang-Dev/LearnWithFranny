e8db6e4ded9eed3bd2e364a8d85628eff89fd2d0
Riley <koppangdev@gmail.com>
2025-01-20 13:59:37 -0700

    Allowing convex to have access to children layouts to be accessed through the application

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import { Outfit } from "next/font/google";
 import "./globals.css";
+import Provider from "./provider";
 
 const font = Outfit({ subsets: ["latin"] });
 
@@ -22,7 +23,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <Provider>{children}</Provider>
+      </body>
     </html>
   );
 }
