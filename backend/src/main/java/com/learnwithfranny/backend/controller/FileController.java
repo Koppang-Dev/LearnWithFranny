c4e35b94d04acfcda0e0e59cfd92090ec45267b3
Riley <koppangdev@gmail.com>
2025-01-22 17:39:00 -0700

    Added enpints for the services created - upload file, delete file and download file

--- backend/src/main/java/com/learnwithfranny/backend/controller/FileController.java
+++ backend/src/main/java/com/learnwithfranny/backend/controller/FileController.java
@@ -1,26 +1,57 @@
 package com.learnwithfranny.backend.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.amazonaws.Response;
+import com.learnwithfranny.backend.service.StorageService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
 @RestController
+@RequestMapping("/file")
 public class FileController {
 
-    @PostMapping("/fileUpload")
-    public ResponseEntity<?> handleFileUpload(@RequestParam("file") MultipartFile file) {
+    @Autowired
+    private StorageService service;
 
-        // Retrieve file name
-        String fileName = file.getOriginalFilename();
 
-        try {
+    // Uploading file endpoint
+    @PostMapping("/upload")
+    public ResponseEntity<String> handleFileUpload(@RequestParam("file") MultipartFile file) {
+        return new ResponseEntity<>(service.uploadFile(file), HttpStatus.OK);
+    }
 
-            return ResponseEntity.ok("File uploaded successfully");
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-        }
-    } 
+    // Downloading file endpoint
+    @GetMapping("/download/{fileName}")
+    public ResponseEntity<ByteArrayResource> downloadFile(@PathVariable String fileName) {
+
+        // Converting to ByteArrayResource
+        byte[] data = service.downloadFile(fileName);
+        ByteArrayResource resource = new ByteArrayResource(data);
+
+        return ResponseEntity.ok()
+                .contentLength(data.length)
+                .header("Content-type", "application/octet-stream")
+                .header("Content-disposition", "attachment; filename=\"" + fileName + "\"")
+                .body(resource);
+    }
+    
+
+    // Deleting the file
+    @DeleteMapping("/delete/{fileName}")
+    public ResponseEntity<String> deleteFile(@PathVariable String fileName) {
+        return new ResponseEntity<>(service.deleteFile(fileName), HttpStatus.OK);
+    }
 }
