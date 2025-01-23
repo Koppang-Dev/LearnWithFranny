d59d7f325c7f01f5939e1f87cc6f85fbb786debf
Riley <koppangdev@gmail.com>
2025-01-22 17:27:08 -0700

    Added methods for uploading and downloading a file from amazon S3 storage

---
+++ backend/src/main/java/com/learnwithfranny/backend/service/StorageService.java
@@ -0,0 +1,81 @@
+package com.learnwithfranny.backend.service;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
+import com.amazonaws.util.IOUtils;
+
+import lombok.extern.slf4j.Slf4j;
+
+
+import lombok.extern.java.Log;
+
+@Service
+@Slf4j
+public class StorageService {
+
+    @Value("${application.bucket.name}")
+    private String bucketName;
+
+    @Autowired
+    private AmazonS3 s3Client;
+
+    // Uploading a file
+    public String uploadFile(MultipartFile file) {
+
+        // Transforming file 
+        File fileObj = convertMultiPartFileToFile(file);
+
+        // Creating unique file name
+        String fileName = System.currentTimeMillis() + "_" + file.getOriginalFilename();
+
+        // Uploads a new object to the specified amazon bucket
+        s3Client.putObject(new PutObjectRequest(bucketName, fileName, fileObj));
+
+        // Deleting file after uploaded to S3 bucket
+        fileObj.delete();
+
+        return "File uploaded: " + fileName;
+    }
+    
+
+    // Downloading a file
+    public byte[] downloadFile(String fileName) {
+        S3Object s3Object = s3Client.getObject(bucketName, fileName);
+        S3ObjectInputStream inputStream = s3Object.getObjectContent();
+
+        // Convert string to byte array
+        try {
+            byte[] content = IOUtils.toByteArray(inputStream);
+            return content;
+        } catch (IOException e) {
+            log.error("Failed to download file from S3 storage", e);
+            return null;
+        }
+
+
+
+
+
+    }
+
+    // Converts Multipart file to regular file
+    private File convertMultiPartFileToFile(MultipartFile file) {
+        File convertedFile = new File(file.getOriginalFilename());
+        try (FileOutputStream fos = new FileOutputStream(convertedFile)) {
+            fos.write(file.getBytes());
+        } catch (IOException e) {
+            log.error("Error converting multipartfile to file", e);
+        }
+        return convertedFile;
+    }
+    
+}
