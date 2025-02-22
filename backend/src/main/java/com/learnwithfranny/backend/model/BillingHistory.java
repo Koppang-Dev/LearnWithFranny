c463582eb5cd2c7c38fc080d68c84cf70741cec0
Riley <koppangdev@gmail.com>
2025-02-22 15:45:55 -0700

    Added the BillingHistory Table

---
+++ backend/src/main/java/com/learnwithfranny/backend/model/BillingHistory.java
@@ -0,0 +1,79 @@
+package com.learnwithfranny.backend.model;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+public class BillingHistory {
+
+    // Primary key ID
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    // Date of the purchase
+    @Column(name = "Date", nullable = false)
+    private Date date;
+
+    // Amount of the purchase
+    @Column(name = "amount")
+    private double amount;
+
+    // Status of the purchase
+    @Column(name = "status")
+    private String status;
+
+    // Joining table with user
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    
+}
